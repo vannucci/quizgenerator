688eea4d6ef327f756efc1956b36f0af193094ed
michaelvannucci <michael.vannucci@wakefern.com>
2019-01-24 15:36:09 -0500

    feat(generator): add in int generator to pick unique questions

--- quizmarkdowngenerator.py
+++ quizmarkdowngenerator.py
@@ -1,8 +1,17 @@
 import json
 from random import randint
+import random
 from string import Template
 import datetime
 import mistune
+
+def shuffle_gen(n):
+  state = dict()
+  for remaining in range(n, 0, -1):
+    i  = random.randrange(remaining)
+    yield state.get(i,i)
+    state[i] = state.get(remaining - 1, remaining - 1)
+    state.pop(remaining - 1, None)
 
 def quizgenerator(numberofquestions = 3,title = "Default",subtitle = "Default subtitle"):
 
@@ -18,8 +27,10 @@ def quizgenerator(numberofquestions = 3,title = "Default",subtitle = "Default su
     topics = ' '.join(data["TADM"]["chapters"]["3"]["topics"])
 
   if(numberofquestions <= len(questionbank)):
+    gen = shuffle_gen(len(questionbank))
     for _ in range(numberofquestions):
-      questionlist.append(questionbank[randint(0,len(questionbank)-1)])
+      print()
+      questionlist.append(questionbank[next(gen)])
       formattedquestionlist = '\n\n1. ' + '\n\n1. '.join(questionlist)
   else:
     print(f"Number of questions requested exceeds size of question bank, requested: {numberofquestions}, available: {len(questionbank)}")
